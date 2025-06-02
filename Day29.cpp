9314486fe6704a447bcec8de957b58a0d940972d
Ravi Kumar pal <rp077216@gmail.com>
2025-06-02 09:44:53 +0530

    Update Day29.cpp

--- Day29.cpp
+++ Day29.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        for(auto &i:image){
-            reverse(i.begin(),i.end());
-            for(int &j:i){
-                j=1-j;
-            }
-        }
-        return image;
+    int findKthLargest(vector<int>& nums, int k) {
+       sort(nums.begin(),nums.end());
+       return nums[nums.size()-k];
     }
 };
