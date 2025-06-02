2ab95b3a9df82338974675421f37757be49bf876
Ravi Kumar pal <rp077216@gmail.com>
2025-06-02 09:25:09 +0530

    Create Day16.cpp

---
+++ Day16.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long a=0,b= sqrt(c);
+        while(a<=b){
+            long long res= a*a + b*b;
+            if(res==c) return true;
+            else if(res<c) a++;
+            else b--;
+        }
+        return false;
+    }
+};
